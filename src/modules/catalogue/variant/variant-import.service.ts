e5602d4deb88890d195f781c83cceb6a16686f4c
Ibrahim Alimson <ialimson@gmail.com>
2025-10-03 17:09:27 +0100

    chore: remove unused PrismaClient import

--- src/modules/catalogue/variant/variant-import.service.ts
+++ src/modules/catalogue/variant/variant-import.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Prisma, PrismaClient } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from '../../../common/prisma/prisma.service';
 
 export interface VariantImportSummary {
